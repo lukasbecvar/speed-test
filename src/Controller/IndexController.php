59540b45dbb7bc9cb0dcc32843bc0469b7ef9ae3
lukasbecvar <becvarlukas99@gmail.com>
2025-08-06 10:28:16 +0200

    added basic speed test function

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,5 +25,41 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render('speedtest.twig');
+    }
+
+    /**
+     * Ping endpoint
+     *
+     * @return Response
+     */
+    #[Route('/ping', methods: ['HEAD'], name: 'ping')]
+    public function ping(): Response
+    {
+        return new Response();
+    }
+
+    /**
+     * Download endpoint
+     *
+     * @return Response
+     */
+    #[Route('/download', methods: ['GET'], name: 'download')]
+    public function download(): Response
+    {
+        $response = new Response(str_repeat('0', 1024 * 1024 * 2)); // 2MB
+        $response->headers->set('Content-Type', 'application/octet-stream');
+        return $response;
+    }
+
+    /**
+     * Upload endpoint
+     *
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/upload', methods: ['POST'], name: 'upload')]
+    public function upload(Request $request): Response
+    {
+        return new Response();
     }
 }
